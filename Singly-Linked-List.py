c16ffabf847dcf9d6d147c8ce9dbccc43609e8c7
T-Davis-86 <118857160+T-Davis-86@users.noreply.github.com>
2023-10-03 13:29:35 -0500

    Add files via upload

--- Singly-Linked-List.py
+++ Singly-Linked-List.py
@@ -1,29 +1,32 @@
-class Node:
+class Node: #Creates a Node instance that contains the nodes value and pointer to the next node
     def __init__(self,value,next_Node = None):
-        self.value = value
-        self.nextNode = next_Node
+        self.value = value # The Nodes value
+        self.nextNode = next_Node # Pointer for the Next Node in the list
 
-class Linked_List:
+class Linked_List: # This class allows for Singly-Linked-List to be created and linked together
     def __init__(self,head = None):
-        self._head = head
-
+        self._head = head # This is the head or start of the list
+    
+    # This is to add a Node to the tail of the Linked List
     def append(self,value):
-        node = Node(value)
-        if self._head is None:
+        node = Node(value) # Creating a new Node with a value
+        if self._head is None:	# This is incase the Linked List is empty
             self._head = node
             return
         currentNode = self._head
-        while True:
+        while True: # This is for iterating through the linked list to find the tail and add the new Node
             if currentNode.nextNode is None:
                 currentNode.nextNode = node
                 break
             else:
                 currentNode = currentNode.nextNode
     
+    # Much like append() this is to "insert" a Node a specific position in the linked list
+    # by tracking the current Nodes next point and the previous Nodes next pointer
     def Insert(self,value,where):
         currentNode = self._head
         target = False
-        prevNode = None
+        prevNode = None 
         while currentNode and target is False:
             if currentNode.value == where:
                 target = True  
@@ -31,32 +34,39 @@ class Linked_List:
                 prevNode = currentNode  
                 currentNode = currentNode.nextNode
         if currentNode is None:
-                print("That number doesn't exist!")
+                print("That number doesn't exist!")	#Incase the node position isn't in the list
                 return
+        elif prevNode is None:
+            node = Node(value)
+            node.nextNode = currentNode	# This allows for a new Node to be added to the front of the list
+            self._head = node
+            return 
         else:
             node = Node(value)
             prevNode.nextNode = node
-            node.nextNode = currentNode
+            node.nextNode = currentNode	# This puts the new Node in the selected spot of the list
             return 
 
-    def popped(self,value):
+    # This allows a Node to be removed from the list by keeping track of the current head and the previous head
+    def popped(self,value): 
         currentNode = self._head
         prevNode = None
         target = False
-        while currentNode and target is False:
+        while currentNode and target is False: # This is iterate through the list for find the node to delete
             if currentNode.value == value:
                 target = True  
             else:
                 prevNode = currentNode  
                 currentNode = currentNode.nextNode      
         if currentNode is None:
-                print("Number isn't in the list!")
+                print("Number isn't in the list!") # This is incase the node doesn't exist
                 return
         if prevNode is None:
-            self._head = currentNode.nextNode
+            self._head = currentNode.nextNode # This is for the number at the front of the list
         else:
             prevNode.nextNode = currentNode.nextNode
-            
+    
+    # This method allows for the list to be printed out        
     def Print(self):
         currentNode = self._head
         while True:
@@ -64,8 +74,10 @@ class Linked_List:
                 print("None")
                 break
             else:
-                print(currentNode.value,"->", end=" ")
+                print(currentNode.value,"->", end=" ") # uses "->" to point to the next node in the list
                 currentNode = currentNode.nextNode
+
+# Using the Class
 
 example = Linked_List()
 example.append(1)
@@ -76,6 +88,8 @@ example.append(5)
 example.append(6)
 example.append(7)
 example.append(8)
+example.append(9)
+example.append(10)
 example.Print()
 example.popped(5)
 example.Print()
@@ -85,4 +99,7 @@ example.popped(2)
 example.Print()
 example.Insert(2,3)
 example.Print()
-
+example.Insert(0,1)
+example.Print()
+example.Insert(-1,0)
+example.Print()
